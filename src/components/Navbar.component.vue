d7962eb3f0bd7488726b016e5c890d9c11ab205f
Jan Babák <babakhonza@gmail.com>
2023-02-04 16:41:50 +0100

    Add: home navigation responsive

--- src/components/Navbar.component.vue
+++ src/components/Navbar.component.vue
@@ -1,35 +1,76 @@
 <template>
-  <v-app-bar app color="primary" dark>
-    <div class="d-flex align-center justify-space-between max-width">
-      <!-- left column -->
-      <div class="d-flex align-center justify-center">
-        <!-- branding -->
-        <Logo class="logo" />
-        <v-toolbar-title>Moneybag</v-toolbar-title>
+  <div>
+    <v-app-bar app color="primary" dark class="ma0">
+      <div class="d-flex align-center justify-space-between max-width">
+        <!-- left column -->
+        <div class="d-flex align-center justify-center">
+          <!-- branding -->
+          <Logo class="logo" />
+          <v-toolbar-title>Moneybag</v-toolbar-title>
 
-        <!-- navigation -->
-        <ul class="nav-links">
-          <li>
-            <a href="#start" class="white--text">Home</a>
-          </li>
-          <li>
-            <a href="#about" class="white--text">About</a>
-          </li>
-          <li>
-            <a href="#contact" class="white--text">Contact</a>
-          </li>
-        </ul>
-      </div>
+          <!-- navigation -->
+          <ul class="nav-links d-none d-md-flex">
+            <li v-for="(link, index) in links" :key="index">
+              <a :href="link.to" class="white--text">{{ link.label }}</a>
+            </li>
+          </ul>
+        </div>
 
-      <!-- right column -->
-      <div class="d-flex align-center justify-center">
-        <v-btn plain :ripple="false"> Sing-in </v-btn>
-        <v-btn color="secondary">
-          <v-icon left>mdi-account-plus-outline</v-icon> Sing-up
-        </v-btn>
+        <!-- right column -->
+        <div class="d-none d-md-flex align-center justify-center">
+          <v-btn plain :ripple="false">
+            <v-icon>mdi-account-lock-open-outline</v-icon>Sing-in
+          </v-btn>
+          <v-btn color="secondary">
+            <v-icon left>mdi-account-plus-outline</v-icon>Sing-up
+          </v-btn>
+        </div>
+
+        <v-app-bar-nav-icon @click="drawer = true" class="d-flex d-md-none" />
       </div>
-    </div>
-  </v-app-bar>
+    </v-app-bar>
+
+    <!-- mobile menu -->
+    <v-navigation-drawer v-model="drawer" app left temporary>
+      <v-list nav dense>
+        <v-list-item-group
+          v-model="drawer"
+          active-class="secondary--text text--accent-4"
+        >
+          <!-- sign btns -->
+          <v-list-item class="mb-4">
+            <v-btn color="secondary">
+              <v-icon left>mdi-account-plus-outline</v-icon>Sing-up
+            </v-btn>
+            <v-btn plain class="ml-1">
+              <v-icon>mdi-account-lock-open-outline</v-icon>Sing-in
+            </v-btn>
+          </v-list-item>
+
+          <v-divider />
+
+          <!-- navigation -->
+          <a
+            v-for="(link, index) in links"
+            :key="index"
+            :href="link.to"
+            class="drawer-link"
+          >
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>{{ link.icon }}</v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title>
+                  {{ link.label }}
+                </v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </a>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
 </template>
 
 <script lang="ts">
@@ -39,24 +80,30 @@ import Logo from "./Logo.component.vue";
 interface NavigationLink {
   to: string;
   label: string;
+  icon: string;
 }
 
 @Component({
   components: { Logo },
 })
 export default class Navbar extends Vue {
+  drawer = false;
+
   links = [
     {
-      to: "/",
+      to: "/#start",
       label: "Home",
+      icon: "mdi-home-outline",
     },
     {
       to: "/#about",
       label: "About",
+      icon: "mdi-information-outline",
     },
     {
       to: "/#contact",
       label: "Contact",
+      icon: "mdi-email-outline",
     },
   ] as NavigationLink[];
 }
@@ -70,11 +117,11 @@ export default class Navbar extends Vue {
 }
 
 .nav-links {
-  display: flex;
   list-style-type: none;
 }
 
-.nav-links li a {
+.nav-links li a,
+.drawer-link {
   text-decoration: none;
 }
 
