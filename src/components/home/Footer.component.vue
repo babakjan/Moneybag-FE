6cad03a5908c4b37e538ce8cec9049212d48252f
Jan Babák <babakhonza@gmail.com>
2023-02-24 21:45:14 +0100

    Add: footer to homepage

---
+++ src/components/home/Footer.component.vue
@@ -0,0 +1,47 @@
+<template>
+  <footer>
+    <ul class="max-width">
+      <li><a href="#start">Home</a></li>
+      <li><a href="#about">About</a></li>
+      <li><a href="#contact">Contacts</a></li>
+    </ul>
+  </footer>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class Footer extends Vue {}
+</script>
+
+<style scoped>
+footer {
+  background: #ddd;
+  width: 100%;
+  padding: 1.5rem 0;
+}
+
+ul {
+  padding: 0;
+  width: 100%;
+  list-style: none;
+  display: flex;
+  gap: 3rem;
+}
+
+ul li a {
+  text-decoration: none;
+  color: black;
+}
+
+ul li a:hover {
+  text-decoration: underline;
+}
+
+@media only screen and (max-width: 960px) {
+  ul {
+    justify-content: center;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,27 @@
 <template>
-  <section id="start" class="max-width">
-    <!-- landing -->
-    <Landing />
+  <div>
+    <section id="start" class="max-width">
+      <!-- landing -->
+      <Landing />
 
-    <!-- feature cards -->
-    <div class="cards">
-      <FeatureCard
-        v-for="(featureCard, index) in featureCards"
-        :key="index"
-        :content="featureCard"
-      />
-    </div>
+      <!-- feature cards -->
+      <div class="cards">
+        <FeatureCard
+          v-for="(featureCard, index) in featureCards"
+          :key="index"
+          :content="featureCard"
+        />
+      </div>
 
-    <!-- about -->
-    <AboutSection />
+      <!-- about -->
+      <AboutSection />
 
-    <!-- contact -->
-    <ContactSection />
-  </section>
+      <!-- contact -->
+      <ContactSection />
+    </section>
+    <!-- footer -->
+    <Footer />
+  </div>
 </template>
 
 <script lang="ts">
@@ -27,9 +31,10 @@ import FeatureCard from "@/components/home/FeatureCard.component.vue";
 import { FeatureCardContent } from "@/components/home/FeatureCard.component.vue";
 import AboutSection from "@/components/home/AboutSection.component.vue";
 import ContactSection from "@/components/home/ContactSection.component.vue";
+import Footer from "@/components/home/Footer.component.vue";
 
 @Component({
-  components: { Landing, FeatureCard, AboutSection, ContactSection },
+  components: { Landing, FeatureCard, AboutSection, ContactSection, Footer },
 })
 export default class Home extends Vue {
   featureCards = [
@@ -66,6 +71,10 @@ export default class Home extends Vue {
   justify-content: center;
   flex-wrap: wrap;
   margin-top: 3rem;
+}
+
+#start {
+  margin-bottom: 8rem;
 }
 
 @media only screen and (max-width: 1250px) {
