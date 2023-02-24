3545738062189ee061f8df0760571cf60ca12f93
Jan Babák <babakhonza@gmail.com>
2023-02-24 17:52:22 +0100

    Add: feature cards on home page

---
+++ src/components/home/FeatureCard.component.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-card class="card">
+    <v-card-title>{{ content.title }}</v-card-title>
+    <v-card-text>{{ content.text }}</v-card-text>
+    <v-icon x-large class="icon" color="primary">{{ content.icon }}</v-icon>
+  </v-card>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from "vue-property-decorator";
+
+interface FeatureCardContent {
+  title: string;
+  text: string;
+  icon: string;
+}
+
+@Component
+export default class FeatureCard extends Vue {
+  @Prop({ required: true }) content!: FeatureCardContent;
+}
+
+export { FeatureCardContent };
+</script>
+
+<style scoped>
+.card {
+  padding: 1rem;
+  width: 25rem;
+}
+
+.icon {
+  width: 100%;
+  padding-bottom: 1rem;
+}
+
+@media only screen and (max-width: 925px) {
+  .card {
+    width: 100%;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,16 @@
 <template>
   <section id="start" class="max-width">
+    <!-- landing -->
     <Landing />
+
+    <div class="cards">
+      <FeatureCard
+        v-for="(featureCard, index) in featureCards"
+        :key="index"
+        :content="featureCard"
+      />
+    </div>
+
     <div id="about" style="height: 100vh">
       <h2 class="pt-16">About</h2>
     </div>
@@ -13,9 +23,46 @@
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import Landing from "@/components/home/Landing.component.vue";
+import FeatureCard from "@/components/home/FeatureCard.component.vue";
+import { FeatureCardContent } from "@/components/home/FeatureCard.component.vue";
 
 @Component({
-  components: { Landing },
+  components: { Landing, FeatureCard },
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  featureCards = [
+    {
+      title: "Transaction Tracking",
+      text:
+        "Track and categorize all your incomes and expenses. Track your spending history " +
+        "Improve your financial health and learn saving money and budgeting.",
+      icon: "mdi-cash-multiple",
+    },
+    {
+      title: "Multiple Accounts",
+      text:
+        "Create multiple accounts for different purposes like Current account, Saving " +
+        "accounts, Investments, Cash...",
+      icon: "mdi-bank-outline",
+    },
+    {
+      title: "Analyze data",
+      text:
+        "Analyze financial reports of your incomes, expenses and cashflow. Visualize balance " +
+        "evolution sor spending by category and more...",
+      icon: "mdi-finance",
+    },
+  ] as FeatureCardContent[];
+}
 </script>
+
+<style scoped>
+.cards {
+  display: flex;
+  gap: 2rem;
+  width: 100%;
+  justify-content: center;
+  flex-wrap: wrap;
+  margin-top: 5rem;
+}
+</style>
